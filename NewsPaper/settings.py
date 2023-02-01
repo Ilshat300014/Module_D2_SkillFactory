3f35d14f27ffe4539c349f105a3fb0b58dd3dd2e
Ilshat <ilshatkhabriev@yandex.ru>
2023-02-01 22:29:32 +0300

    Create accounts, add to settings new and account apps

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'news',
+    'accounts',
 ]
 
 ROOT_URLCONF = 'NewsPaper.urls'
