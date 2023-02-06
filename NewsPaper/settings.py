73366530833b7d421a931ec50d15b72988c12b67
Ilshat <ilshatkhabriev@yandex.ru>
2023-02-06 20:01:27 +0300

    Add models

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -47,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'news',
-    'accounts',
 ]
 
 ROOT_URLCONF = 'NewsPaper.urls'
