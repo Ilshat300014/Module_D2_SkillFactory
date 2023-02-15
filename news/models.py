86c3ab5baf4521485bda2ee0a59bcfcb8158285e
Ilshat300014 <ilshatkhabriev@yandex.ru>
2023-02-15 14:05:33 +0300

    Create model Comment

--- news/models.py
+++ news/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-# from django.contrib.auth import models.Us
+from django.contrib.auth.models import User
 
 class Author(models.Model):
-    # user = models.OneToOneField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_rating = models.IntegerField(default=0)
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, unique=True)
 
 class Post(models.Model):
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     choice = models.CharField(max_length=3)
     create_date = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(Category)
@@ -20,6 +20,15 @@ class Post(models.Model):
 class PostCategory(models.Model):
     post = models.ForeignKey(Post)
     category = models.ForeignKey(Category)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    comment_text = models.TextField()
+    date_of_create_comment = models.DateTimeField()
+    comment_rating = models.IntegerField()
+
+
 # Create your models here.
 # sauto_now
 # Если True, то автоматически устанавливает в это поле текущую дату каждый раз при сохранении объекта.
